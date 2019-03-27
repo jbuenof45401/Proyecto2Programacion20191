6b8f95161aae61c3dc163130cef59a12241ac8f1
tatarobl <fmezacr96407@universidadean.edu.co>
2019-03-26 20:01:25 -0500

    mi partesita despues sigo

---
+++ dev.py
@@ -0,0 +1,14 @@
+
+def obtener_complementos(lista_adn):
+    '''
+
+    :param lista_adn:
+    :return:
+    '''
+
+def unir_cadena(lista_adn):
+    pass
+
+
+def complementar_cadenas(lista_adn):
+    pass
