302862373f94fe7c7ded0c05108da02720e02ac7
MarlonGamba17 <mgambav18183@universidadean.edu.co>
2019-04-01 00:09:43 -0500

    avances

--- adn.py
+++ adn.py
@@ -1,9 +1,4 @@
 def obtener_complemento(base):
-    '''
-
-    :param base:
-    :return:
-    '''
     # retorna caracter
     pass
 
@@ -18,19 +13,7 @@ def calcular_correspondencia(adn1, adn2):
 
 
 def corresponden(adn1, adn2):
-    '''
-    (str, str) -> bool: valida la correspondencia entre dos cadenas
 
-    >>> corresponden('AGATA', 'TCTAT')
-    True
-
-    >>> corresponden('AGATA', 'GACCG')
-    False
-
-    :param adn1: primera cadena a comparar
-    :param adn2: segunda cadena a comparar
-    :return: bool: validacion de la correspondencia
-    '''
     if adn2 == generar_cadena_complementaria(adn1):
         return True
     else:
@@ -38,39 +21,14 @@ def corresponden(adn1, adn2):
 
 
 def es_cadena_valida(adn):
-    '''
-    (str) -> bool: validar si todos las bases son validas en la cadena
 
-    >>> es_cadena_valida(AGATA)
-    True
-
-    >>> es_cadena_valida(AGATS)
-    False
-
-    :param adn: str: cadena a validar
-    :return: bool: validacion de la cadena
-    '''
     if adn == obtener_complemento(base):
         return True
     else:
         return False
 
 def es_base(caracter):
-    '''
-    (char) -> bool: la funcion es base
 
-    >>> es_base('B')
-    False
-
-    >>> es_base('A')
-    True
-
-    >>> es_base('AGATA')
-    ValueError
-
-    :param caracter: base a validar
-    :return: bool: validacion de la base
-    '''
     if len(caracter) != 1
         return ValueError(caracter + 'tiene más de un caracter');
     elif caracter in base:
@@ -79,19 +37,7 @@ def es_base(caracter):
         return False
 
 def es_subcadena(adn1, adn2):
-    '''
-    (str, str) -> bool: Una funcion es subacdena
 
-    >>> es_subcadena('AGA', 'AGATA')
-    True
-
-    >>> es_subcadena('AGA', 'GCAC')
-    False
-
-    :param adn1: str: primera cadena a comparar
-    :param adn2: str: segunda cadena a comparar
-    :return: bool: validacion y verificacion si una es una subcdena de la otra
-    '''
     if adn1 in adn2:
         return True;
     elif adn2 in adn1:
