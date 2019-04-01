f550b880143e6afb1f05006d10d7cd4a2472d745
MarlonGamba17 <mgambav18183@universidadean.edu.co>
2019-03-31 23:57:34 -0500

    avances

--- adn.py
+++ adn.py
@@ -18,21 +18,86 @@ def calcular_correspondencia(adn1, adn2):
 
 
 def corresponden(adn1, adn2):
-    # retorna Bool
-    pass
+    '''
+    (str, str) -> bool: valida la correspondencia entre dos cadenas
+
+    >>> corresponden('AGATA', 'TCTAT')
+    True
+
+    >>> corresponden('AGATA', 'GACCG')
+    False
+
+    :param adn1: primera cadena a comparar
+    :param adn2: segunda cadena a comparar
+    :return: bool: validacion de la correspondencia
+    '''
+    if adn2 == generar_cadena_complementaria(adn1):
+        return True
+    else:
+        return False
 
 
 def es_cadena_valida(adn):
-    pass
+    '''
+    (str) -> bool: validar si todos las bases son validas en la cadena
 
+    >>> es_cadena_valida(AGATA)
+    True
+
+    >>> es_cadena_valida(AGATS)
+    False
+
+    :param adn: str: cadena a validar
+    :return: bool: validacion de la cadena
+    '''
+    if adn == obtener_complemento(base):
+        return True
+    else:
+        return False
 
 def es_base(caracter):
-    pass
+    '''
+    (char) -> bool: la funcion es base
 
+    >>> es_base('B')
+    False
+
+    >>> es_base('A')
+    True
+
+    >>> es_base('AGATA')
+    ValueError
+
+    :param caracter: base a validar
+    :return: bool: validacion de la base
+    '''
+    if len(caracter) != 1
+        return ValueError(caracter + 'tiene más de un caracter');
+    elif caracter in base:
+        return True
+    else:
+        return False
 
 def es_subcadena(adn1, adn2):
-    pass
+    '''
+    (str, str) -> bool: Una funcion es subacdena
 
+    >>> es_subcadena('AGA', 'AGATA')
+    True
+
+    >>> es_subcadena('AGA', 'GCAC')
+    False
+
+    :param adn1: str: primera cadena a comparar
+    :param adn2: str: segunda cadena a comparar
+    :return: bool: validacion y verificacion si una es una subcdena de la otra
+    '''
+    if adn1 in adn2:
+        return True;
+    elif adn2 in adn1:
+        return True
+    else:
+        return False
 
 def reparar_dano(adn, base):
     pass
