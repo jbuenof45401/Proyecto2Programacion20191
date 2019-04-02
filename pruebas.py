abd67833cb7ad06c095d7894219e6d7ac8b29b30
MarlonGamba17 <mgambav18183@universidadean.edu.co>
2019-04-02 02:30:33 -0500

    sincrnizacion

--- pruebas.py
+++ pruebas.py
@@ -7,21 +7,25 @@ class pruebas(unittest.TestCase):
         self.assertEqual(ADN.corresponden(AGATA, GACCG),False)
         self.assertEqual(ADN.corresponden(TACA, ATGT), True)
         self.assertEqual(ADN.corresponden(ATACA, GCC), False)
+        self.assertEqual(ADN.corresponden(ATACA, ZPQW), 'Alguna cadena no es valida')
     def test_es_cadena_valida(self):
         self.assertEqual(ADN.es_cadena_valida(AGATA), True)
         self.assertEqual(ADN.es_cadena_valida(AGATS), False)
         self.assertEqual(ADN.es_cadena_valida(CANTAR), False)
         self.assertEqual(ADN.es_cadena_valida(TAGA), True)
+        self.assertEqual(ADN.es_cadena_valida(COMBO), 'Ingrese una cadena valida')
     def test_es_base(self):
         self.assertEqual(ADN.es_base(A), True)
         self.assertEqual(ADN.es_base(B), False)
         self.assertEqual(ADN.es_base(C), True)
         self.assertEqual(ADN.es_base(D), False)
+        self.assertEqual(ADN.es_base(DRE), 'DRE tiene más de un caracter')
     def test_es_subcadena(self):
         self.assertEqual(ADN.es_subcadena(TAGA, TAG), True)
         self.assertEqual(ADN.es_subcadena(GATA, ATCT), False)
         self.assertEqual(ADN.es_subcadena(TAGA, CCTG), False)
         self.assertEqual(ADN.es_subcadena(ATCT, AT), True)
+        self.assertEqual(ADN.es_subcadena(ATCT, DQ), 'Las cadenas no son validas')
 
 if __name__== 'main':
     unittest.main()
